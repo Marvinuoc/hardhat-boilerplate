a5b0e6a06f4955ec4f3878d6bc2e9561026b9858
Marvin Sasso <1136895@student.uoc.cw>
2024-05-29 02:19:13 -0400

    zf

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,21 +1,22 @@
+const { ethers } = require("hardhat");
+
 async function main() {
-    const [deployer] = await ethers.getSigners();
-    console.log("Deploying contracts with the account:", deployer.address);
-  
-    const balance = await deployer.getBalance();
-    console.log("Account balance:", balance.toString());
-  
-    const LegalProvenance = await ethers.getContractFactory("LegalProvenance");
-    const legalProvenance = await LegalProvenance.deploy();
-    await legalProvenance.deployed();
-  
-    console.log("LegalProvenance deployed to:", legalProvenance.address);
-  }
-  
-  main()
-    .then(() => process.exit(0))
-    .catch((error) => {
-      console.error(error);
-      process.exit(1);
-    });
-  
+  const [deployer] = await ethers.getSigners();
+  console.log("Deploying contracts with the account:", deployer.address);
+
+  const balance = await deployer.getBalance();
+  console.log("Account balance:", balance.toString());
+
+  const LegalProvenance = await ethers.getContractFactory("LegalProvenance");
+  const legalProvenance = await LegalProvenance.deploy();
+  await legalProvenance.deployed();
+
+  console.log("LegalProvenance deployed to:", legalProvenance.address);
+}
+
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
