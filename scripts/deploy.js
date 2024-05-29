73147d764e1143aa707e2034279939063ecdc177
Marvin Sasso <1136895@student.uoc.cw>
2024-05-29 00:49:45 -0400

    version1.9

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,20 +1,21 @@
 async function main() {
     const [deployer] = await ethers.getSigners();
     console.log("Deploying contracts with the account:", deployer.address);
-
+  
     const balance = await deployer.getBalance();
     console.log("Account balance:", balance.toString());
-
+  
     const LegalProvenance = await ethers.getContractFactory("LegalProvenance");
     const legalProvenance = await LegalProvenance.deploy();
     await legalProvenance.deployed();
-
+  
     console.log("LegalProvenance deployed to:", legalProvenance.address);
-}
-
-main()
+  }
+  
+  main()
     .then(() => process.exit(0))
     .catch((error) => {
-        console.error(error);
-        process.exit(1);
+      console.error(error);
+      process.exit(1);
     });
+  
