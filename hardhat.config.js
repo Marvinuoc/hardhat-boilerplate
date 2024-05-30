a4422052c8930208db32b0cd4a6b251aae83c132
Marvin Sasso <1136895@student.uoc.cw>
2024-05-29 21:14:36 -0400

    till contracts deployment

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,10 +1,18 @@
 require("@nomicfoundation/hardhat-toolbox");
 
 module.exports = {
-  solidity: "0.8.24",
+  solidity: {
+    version: "0.8.0",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 200
+      }
+    }
+  },
   networks: {
-    localhost: {
-      url: "http://127.0.0.1:8545"
+    hardhat: {
+      chainId: 1337
     }
   }
 };
